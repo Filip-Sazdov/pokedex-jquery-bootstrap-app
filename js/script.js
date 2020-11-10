5f852c90ea5e808fc449a9dea6fce0c0012e08a4
Filip Sazdov <45643632+Filip-Sazdov@users.noreply.github.com>
2020-11-10 16:07:26 -0500

    Loaded data and added to DOM.

--- js/script.js
+++ js/script.js
@@ -8,23 +8,28 @@ let pokemonRepository = (function () {
 	function getPokemonListContents() {
 		return pokemonList;
 	}
-	// Loads initial object of name and detailsUrl and adds it to the pokemonList
+	// // Loads initial object of name and detailsUrl and adds it to the pokemonList
 	function loadDataFromApi() {
-		$.ajax(apiUrl).done((response) => {
-			response.results.forEach((item) => {
-				let pokemon = {
-					name: item.name,
-					detailsUrl: item.url,
-				};
-				addToPokemonList(pokemon);
-			});
-		});
+		return fetch(apiUrl)
+			.then((response) => response.json())
+			.then((data) => {
+				data.results.forEach((item) => {
+					let pokemon = {
+						name: item.name,
+						detailsUrl: item.url,
+					};
+					addToPokemonList(pokemon);
+				});
+			})
+			.catch((err) => console.error(err));
 	}
 
-	// Loads the auxiliary details as key/value pairs for each Pokemon and stores it in the pokemonList
+	// // Loads the auxiliary details as key/value pairs for each Pokemon and stores it in the pokemonList
 	function loadDetails(listItem) {
 		let url = listItem.detailsUrl;
-		$.ajax(url).done((data) => {
+		return fetch(url)
+			.then((response) => response.json())
+			.then((data) => {
 				listItem.abilities = data.abilities;
 				listItem.height = data.height;
 				listItem.weight = data.weight;
@@ -40,18 +45,58 @@ let pokemonRepository = (function () {
 				});
 			})
 			.catch((err) => console.error(err));
-    }
-    
-    
+	}
+
+	function addToDom(pokemon) {
+		loadDetails(pokemon).then(() => {
+			let pokemonGrid = $('.row');
+			let cardContainer = $('<div></div>').addClass('col col-sm-6 col-md-4');
+			let card = $('<div></div>').addClass('card text-center bg-light my-2 flex-grow-0 border border-warning');
+			// card.attr({
+			// 	maxWidth: '33%',
+			// 	minWidth: '200px',
+			// });
+
+			let cardImg = $('<img>');
+			cardImg.addClass('card-img-top card-image w-50 h-50 mx-auto my-3');
+			cardImg.attr({
+				src: pokemon.imageUrlAnimation,
+				alt: 'gif of Pokemon',
+			});
+
+			let cardBody = $('<div></div>');
+			cardBody.addClass('card-body');
+
+			let modalButton = $('<button></button>');
+			modalButton.text(pokemon.name);
+			modalButton.addClass('btn btn-primary btn-lg text-capitalize text-warning bg-secondary');
+			modalButton.attr('type', 'button');
+			// modalButton.setAttribute('data-toggle', 'modal');
+			// modalButton.setAttribute('data-target', '#ModalCenter');
+
+			cardBody.append(modalButton);
+			card.append(cardImg);
+			card.append(cardBody);
+			cardContainer.append(card);
+			pokemonGrid.append(cardContainer);
+
+			// modalButton.addEventListener('click', function () {
+			// 	showModal(pokemon);
+			// });
+		});
+	}
 
 	return {
 		addToPokemonList: addToPokemonList,
 		getPokemonListContents: getPokemonListContents,
 		loadDataFromApi: loadDataFromApi,
 		loadDetails: loadDetails,
+		addToDom: addToDom,
 	};
 })();
 
-pokemonRepository.loadDataFromApi();
-
-
+pokemonRepository.loadDataFromApi().then(function () {
+	pokemonRepository.getPokemonListContents().forEach((pokemon) => {
+		pokemonRepository.addToDom(pokemon);
+	});
+});
