106162ec0e79fe6bd76cad11686ef043575c0b2c
Filip Sazdov <45643632+Filip-Sazdov@users.noreply.github.com>
2020-11-11 15:11:04 -0500

    Fixed card and text esthetics.

--- js/script.js
+++ js/script.js
@@ -1,6 +1,6 @@
 let pokemonRepository = (function () {
 	let pokemonList = [];
-	let apiUrl = 'https://pokeapi.co/api/v2/pokemon/?limit=20';
+	let apiUrl = 'https://pokeapi.co/api/v2/pokemon/?limit=150';
 
 	function addToPokemonList(pokemonFromApi) {
 		pokemonList.push(pokemonFromApi);
@@ -10,8 +10,7 @@ let pokemonRepository = (function () {
 	}
 	// // Loads initial object of name and detailsUrl and adds it to the pokemonList
 	function loadDataFromApi() {
-		return fetch(apiUrl)
-			.then((response) => response.json())
+		return $.ajax(apiUrl)
 			.then((data) => {
 				data.results.forEach((item) => {
 					let pokemon = {
@@ -27,8 +26,7 @@ let pokemonRepository = (function () {
 	// // Loads the auxiliary details as key/value pairs for each Pokemon and stores it in the pokemonList
 	function loadDetails(listItem) {
 		let url = listItem.detailsUrl;
-		return fetch(url)
-			.then((response) => response.json())
+		return $.ajax(url)
 			.then((data) => {
 				listItem.abilities = data.abilities;
 				listItem.height = data.height;
@@ -50,29 +48,31 @@ let pokemonRepository = (function () {
 	function addToDom(pokemon) {
 		loadDetails(pokemon).then(() => {
 			let pokemonGrid = $('.row');
-			let cardContainer = $('<div></div>').addClass('col col-sm-6 col-md-4');
-			let card = $('<div></div>').addClass('card text-center bg-light my-2 flex-grow-0 border border-warning');
-			// card.attr({
-			// 	maxWidth: '33%',
-			// 	minWidth: '200px',
-			// });
+			let cardContainer = $('<div></div>').addClass('col-xs col-sm-6 col-md-4');
+			let card = $('<div></div>').addClass('card text-center bg-light my-2 border border-warning');
 
-			let cardImg = $('<img>');
-			cardImg.addClass('card-img-top card-image w-50 h-50 mx-auto my-3');
-			cardImg.attr({
-				src: pokemon.imageUrlAnimation,
-				alt: 'gif of Pokemon',
-			});
+			let cardImg = $('<img>')
+				.addClass('card-img-top mx-auto my-3')
+				.attr({
+					src: pokemon.imageUrlAnimation,
+					alt: 'gif of Pokemon',
+				})
+				.css({
+					width: '150px',
+					height: '150px',
+				});
 
 			let cardBody = $('<div></div>');
 			cardBody.addClass('card-body');
 
-			let modalButton = $('<button></button>');
-			modalButton.text(pokemon.name);
-			modalButton.addClass('btn btn-primary btn-lg text-capitalize text-warning bg-secondary');
-			modalButton.attr('type', 'button');
-			// modalButton.setAttribute('data-toggle', 'modal');
-			// modalButton.setAttribute('data-target', '#ModalCenter');
+			let modalButton = $('<button></button>')
+				.text(pokemon.name)
+				.addClass('btn btn-primary btn-lg text-capitalize text-warning bg-secondary border border-warning')
+				.attr({
+					type: 'button',
+					'data-toggle': 'modal',
+					'data-target': '#ModalCenter',
+				});
 
 			cardBody.append(modalButton);
 			card.append(cardImg);
@@ -80,10 +80,52 @@ let pokemonRepository = (function () {
 			cardContainer.append(card);
 			pokemonGrid.append(cardContainer);
 
-			// modalButton.addEventListener('click', function () {
-			// 	showModal(pokemon);
-			// });
+			modalButton.on('click', function () {
+				showModal(pokemon);
+			});
 		});
+	}
+	function showModal(pokemon) {
+		let modalBody = $('.modal-body');
+		modalBody.addClass('text-center');
+
+		let modalTitle = $('.modal-title');
+
+		modalBody.empty();
+		modalTitle.empty();
+
+		let name = $('<h1></h1>');
+		name.text(pokemon.name);
+		name.addClass('text-capitalize');
+
+		let spriteElement = $('<img>');
+		spriteElement.attr('src', pokemon.imageUrl);
+		spriteElement.addClass('text-center w-50');
+
+		let capitalisedName = pokemon.name[0].toUpperCase().concat(pokemon.name.slice(1));
+		let stringifiedTypes = pokemon.types.join(', and ');
+		let stringifiedAbilities = pokemon.abilities.join(', and ');
+		// The API provided an erroneus value for height, value is missing a "." to denote height in meters (example: 11, instead of 1.1) so I corrected it with the below code after checking the true height on pokemon.com which is displayed there in feet.
+		let correctHeight = function () {
+			heightFromApi = pokemon.height.toString();
+
+			if (heightFromApi.length < 2) {
+				heightFromApi = '0.' + heightFromApi;
+			} else {
+				heightFromApi = heightFromApi[0] + '.' + heightFromApi[1];
+			}
+			return heightFromApi;
+		};
+
+		let paragraph = $('<p></p>');
+		paragraph.text(
+			`${capitalisedName} is a Pokemon of type[s]: ${stringifiedTypes} and has a height of ${correctHeight()} meters. Its abilities are: ${stringifiedAbilities}.`
+		);
+		paragraph.addClass('mt-3');
+
+		modalTitle.append(name);
+		modalBody.append(spriteElement);
+		modalBody.append(paragraph);
 	}
 
 	return {
